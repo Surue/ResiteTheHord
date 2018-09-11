38251f96644c65087bce0251f5cb6a953fec43d8
Nicolas Schneider <nicolas06062000@hotmail.com>
2018-09-11 14:42:52 +0200

    Implement new way to draw tilemap

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class cameraController : MonoBehaviour {
     
     public GameObject focusedObject;
-    Vector2 aimPoint = Vector2.zero;
 
     // Use this for initialization
     void Start () {
@@ -13,19 +12,10 @@ public class cameraController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 	    if (focusedObject) {
-	        Vector2 newAimPoint = transform.position;
-
-	        Vector2 lerpedAimPoint = Vector2.Lerp(transform.position, focusedObject.transform.up * 3 + focusedObject.transform.position, Time.deltaTime * 6.5f);
-            Debug.Log(transform.position);
-            Debug.Log(focusedObject.transform.up * 3 + focusedObject.transform.position);
-            Debug.Log("================");
-
-	        transform.position = lerpedAimPoint;
-	        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
-
-	        aimPoint = focusedObject.transform.up * 3;
+            transform.position = Vector2.Lerp(transform.position, focusedObject.transform.up * 3 + focusedObject.transform.position, Time.deltaTime * 6.5f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
         }
 	}
 }
